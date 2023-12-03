5b6f9f8476e786606178dbab944edf970e9d2b9b
Tim <tmeier42+github@gmail.com>
2023-12-03 14:58:20 +0200

    cleanup and speedup

--- days/3/solution.js
+++ days/3/solution.js
@@ -1,37 +1,62 @@
 module.exports = function (input) {
-    input = '.' + input.trim().split(/\r?\n/).join('.\n.') + '.'
-    const lineLength = input.search(/\r?\n/);
-    input = '.' * lineLength + '\n' + input + '\n' + '.' * lineLength
-    const grid = input.replaceAll(/\r?\n/g, '').trim();
-    const symbols = [...grid.matchAll(/[^\d\.]/g)];
-    const labels = [...grid.matchAll(/\d{1,3}/g)];
-    gears = {};
     let part1 = 0;
-    for (const label of labels) {
-        const lx = Math.floor(label.index % lineLength);
-        const ly = Math.floor(label.index / lineLength);
-
-        const ll = label[0].length;
-        let isLabel = false;
-        for (const symbol of symbols) {
-            const sx = Math.floor(symbol.index % lineLength);
-            const sy = Math.floor(symbol.index / lineLength);
-            if (sx >= lx - 1 && sx <= lx + ll && sy >= ly - 1 && sy <= ly + 1) {
-                isLabel = true;
-                if (symbol[0] === '*') {
-                    if (gears[symbol.index]) {
-                        gears[symbol.index].push(label[0]);
-                    } else {
-                        gears[symbol.index] = [label[0]];
+    let part2 = 0;
+    const lineLength = input.search(/\n/) + 1;
+    let currentNumber = 0;
+    let currentNumberIndex = 0;
+    let numberSearchStart = 0;
+    const numbers = [];
+    for (let i = 0; i < input.length; i++) {
+        const char = input.charCodeAt(i);
+        // check if digit
+        if (char >= 48 && char <= 57) {
+            if (currentNumber === 0) {
+                currentNumberIndex = i;
+            }
+            currentNumber = currentNumber * 10 + char - 48;
+        } else {
+            if (currentNumber !== 0) {
+                numbers.push({value: currentNumber, seen: false, start: currentNumberIndex, end: currentNumberIndex + Math.floor(Math.log10(currentNumber))});
+                currentNumber = 0;
+            }
+        }
+    }
+    
+    for (let i = 0; i < input.length; i++) {
+        const char = input.charCodeAt(i);
+        // check if symbol
+        if (char === 61 || char === 64 || char === 47 || (char >= 35 && char <= 45)) {
+            const neighbourhoodStart = i - lineLength - 1;
+            const neighbourhoodEnd = i + lineLength + 1;
+            let gearLinks = 0;
+            let gearRatio = 1;
+            for (let n = numberSearchStart; n < numbers.length; n++) {
+                const number = numbers[n];
+                if (neighbourhoodStart > number.end) {
+                    numberSearchStart = n + 1;
+                    continue;
+                }
+                if (neighbourhoodEnd < number.start) {
+                    break;
+                }
+                if (
+                    number.end === i - 1 ||
+                    number.start === i + 1 ||
+                    (number.end >= i - lineLength - 1 && number.start <= i - lineLength + 1) ||
+                    (number.end >= i + lineLength - 1 && number.start <= i + lineLength + 1)
+                ) {
+                    gearLinks += 1;
+                    gearRatio *= number.value;
+                    if (number.seen === false) {
+                        number.seen = true;
+                        part1 += number.value;
                     }
                 }
             }
+            if (gearLinks === 2) {
+                part2 += gearRatio;
+            }
         }
-        if (isLabel) part1 += parseInt(label[0]);
     }
-    const part2 = Object.values(gears)
-        .filter((v) => v.length === 2)
-        .map((v) => v[0] * v[1])
-        .reduce((sum, v) => sum + v, 0)
     return [part1, part2];
 }
