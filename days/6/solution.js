0201aa9bbe46b5855d79a0f47a9e14a4fa8c8e91
Tim <tmeier42+github@gmail.com>
2023-12-06 08:52:57 +0200

    oops

--- days/6/solution.js
+++ days/6/solution.js
@@ -5,12 +5,13 @@ module.exports = function (input) {
     let part1 = 1;
 
     const high = (time, distance) => Math.ceil((-time - (time**2 - 4*distance)**0.5)/(-2))
-    const low = (time, distance) => Math.ceil((-time + (time**2 - 4*distance)**0.5)/(-2))
+    const low = (time, distance) => Math.floor((-time + (time**2 - 4*distance)**0.5)/(-2))
     for (let i = 0; i < times.length; i++) {
-        part1 *= high(times[i], distances[i]) - low(times[i], distances[i]);
+        console.log(high(times[i], distances[i]), low(times[i], distances[i]))
+        part1 *= high(times[i], distances[i]) - low(times[i], distances[i]) - 1;
     }
     const time = parseInt(lines[0].split(/\s+/).slice(1).join(''))
     const distance = parseInt(lines[1].split(/\s+/).slice(1).join(''))
-    const part2 = high(time, distance) - low(time, distance);
+    const part2 = high(time, distance) - low(time, distance) - 1;
     return [part1, part2];
 }
