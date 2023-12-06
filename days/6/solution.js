4792f88ffbe50d29b7253dcee48e8354f9851f60
Tim <tmeier42+github@gmail.com>
2023-12-06 08:41:02 +0200

    quick maffs

---
+++ days/6/solution.js
@@ -0,0 +1,16 @@
+module.exports = function (input) {
+    const lines = input.trim().split('\n');
+    const times = lines[0].split(/\s+/).slice(1).map((s) => parseInt(s));
+    const distances = lines[1].split(/\s+/).slice(1).map((s) => parseInt(s));
+    let part1 = 1;
+
+    const high = (time, distance) => Math.ceil((-time - (time**2 - 4*distance)**0.5)/(-2))
+    const low = (time, distance) => Math.ceil((-time + (time**2 - 4*distance)**0.5)/(-2))
+    for (let i = 0; i < times.length; i++) {
+        part1 *= high(times[i], distances[i]) - low(times[i], distances[i]);
+    }
+    const time = parseInt(lines[0].split(/\s+/).slice(1).join(''))
+    const distance = parseInt(lines[1].split(/\s+/).slice(1).join(''))
+    const part2 = high(time, distance) - low(time, distance);
+    return [part1, part2];
+}
