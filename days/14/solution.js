3c6d5fb0bef647f06935edfa6bda85b7bc4ded49
timmeh42 <tmeier42+github@gmail.com>
2023-12-14 19:41:46 +0200

    different but the same

--- days/14/solution.js
+++ days/14/solution.js
@@ -1,155 +1,105 @@
 module.exports = function (input) {
-    const grid = [[]];
+    let grid = [];
     let x = 0;
     let y = 0;
-    const gridMap = new Map();
-    let width = 0;
     for (const char of input.trim()) {
         if (char === '\n') {
-            if (width === 0) {
-                width = x;
-            }
-            x = 0;
             y++;
-            grid.push([]);
+            x = 0;
         } else {
-            const coordHash = x + y * width;
-            if (char !== '.') {
-                gridMap.set(coordHash, char);
+            if (y === 0) {
+                grid.push([]);
             }
-
-            grid[y].push(char);
+            let c = 0;
+            if (char === 'O') c = 1;
+            if (char === '#') c = 2;
+            grid[x].push(c);
             x++;
         }
     }
     const height = y + 1;
+    const width = x;
 
-
-    let part1;
-    const directions = [[0, -1], [-1, 0], [0, 1], [1, 0]];
-    let direction = [0, -1];
-    const memory = [];
+    let tiltedGrid = [];
+    let part1 = 0;
+    let part2 = 0;
     const rounds = 4e9;
+    let score = 0;
+    const memory = [];
     let looped = false;
     for (let n = 0; n < rounds; n++) {
-        direction = directions[n % 4];
-        for (const [coordHash, char] of gridMap.entries()) {
-            if (char === 'O') {
-                const x = coordHash % width;
-                const y = Math.floor(coordHash / width);
-                let nx = x;
-                let ny = y;
-                let lastEmpty;
-                while (true) {
-                    nx += direction[0];
-                    ny += direction[1];
-                    if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
-                        const nCoordHash = nx + ny * width;
-                        const gridCheck = gridMap.get(nCoordHash)
-                        if (gridCheck) {
-                            if (gridCheck === '#') {
-                                break;
-                            }
-                        } else {
-                            lastEmpty = nCoordHash;
-                        }
-                    } else {
-                        break
-                    }
+        let hash = '';
+        tiltedGrid = [];
+        for (let x = 0; x < width; x++) {
+            tiltedGrid.push([]);
+            let rockCount = 0;
+            let spaceCount = 0;
+            let brickCount = 0;
+            let compactedY = 0;
+            for (let y = 0; y < height; y++) {
+                const piece = grid[x][y];
+                if (piece !== 2 && brickCount > 0) {
+                    tiltedGrid[x].push(...Array(rockCount).fill(1), ...Array(spaceCount).fill(0), ...Array(brickCount).fill(2));
+                    if (rockCount > 0) hash += rockCount;
+                    if (spaceCount > 0) hash += spaceCount;
+                    if (brickCount > 0) hash += brickCount;
+                    score += (2 * (height - compactedY) - rockCount + 1) * rockCount / 2
+                    rockCount = 0;
+                    spaceCount = 0;
+                    brickCount = 0;
+                    compactedY = y;
                 }
-                if (lastEmpty !== undefined) {
-                    // console.log('moving to', lastEmpty)
-                    gridMap.set(lastEmpty, char);
-                    gridMap.delete(coordHash);
-                }
-    
+                if (piece === 0) spaceCount++;
+                else if (piece === 1) rockCount++;
+                else if (piece === 2) brickCount++;
             }
+            tiltedGrid[x].push(...Array(rockCount).fill(1), ...Array(spaceCount).fill(0), ...Array(brickCount).fill(2));
+            if (rockCount > 0) hash += rockCount;
+            if (spaceCount > 0) hash += spaceCount;
+            if (brickCount > 0) hash += brickCount;
+            score += (2 * (height - compactedY) - rockCount + 1) * rockCount / 2
         }
-        const hash = getHash(gridMap, width, height);
-        const lastSeen = memory.indexOf(hash)
+        if (n === 0) {
+            part1 = score;
+        }
+
+        const lastSeen = memory.indexOf(hash);
         if (looped === false && lastSeen !== -1) {
             const loopLength = n - lastSeen;
-            // console.log(hash, lastSeen, loopLength, n, Math.floor((rounds - n) / loopLength) * loopLength)
             n += (Math.floor((rounds - n) / loopLength) - 1) * loopLength;
-            // console.log('looping', hash)
             looped = true;
         }
-        // if (getPart(gridMap, width, height) === 64) {
-        //     console.log(hash, n)
-        // }
-        // if (looped === true) {
-        //     console.log(hash, n, getPart(gridMap, width, height))
-        // }
         memory.push(hash);
-        if (part1 === undefined) {
-            part1 = getPart(gridMap, width, height);
-        }
-        // console.log(direction)
-        logMap(gridMap, width, height);
-    }
-    const part2 = getPart(gridMap, width, height);
 
-    // for (let y = 0; y < grid.length; y++) {
-    //     for (let x = 0; x < grid[y].length; x++) {
-    //         let ty = y - 1;
-    //         if (grid[y][x] === 'O') {
-    //             while(ty >= 0 && grid[ty][x] === '.') {
-    //                 grid[ty][x] = 'O';
-    //                 grid[ty+1][x] = '.';
-    //                 ty--;
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let part12 = 0;
-    // for (let y = 0; y < grid.length; y++) {
-    //     for (let x = 0; x < grid[y].length; x++) {
-    //         if (grid[y][x] === 'O') {
-    //             part12 += grid.length - y;
-    //         }
-    //     }
-    // }
-    // console.log(grid.map((line) => line.join('') + '            '))
-    return [part1, part2];
-}
-
-function getPart(gridMap, width, height) {
-    let part = 0;
-    for (const [coordHash, char] of gridMap.entries()) {
-        if (char === 'O') {
-            const y = Math.floor(coordHash / width);
-            part += height - y;
-        }
-    }
-    return part;
-}
-
-function getHash(gridMap, width, height) {
-    let hash = '';
-    for (const [coordHash, char] of [...gridMap.entries()].sort((a, b) => a[0] - b[0])) {
-        if (char === 'O') {
-            // const x = coordHash % width;
-            // const y = Math.floor(coordHash / width);
-            // hash += x + y + x * y;
-            hash += coordHash;
-        }
-    }
-    return hash;
-}
-
-function logMap(gridMap, width, height) {
-    let grid = '';
-    for (let y = 0; y < height; y++) {
+        const rotatedGrid = [];
         for (let x = 0; x < width; x++) {
-            const hash = x + y * width;
-            if (gridMap.has(hash)) {
-                grid += gridMap.get(hash);
-            } else {
-                grid += '.';
+            rotatedGrid.push([]);
+            for (let y = 0; y < height; y++) {
+                if (tiltedGrid[y][width - x - 1] === undefined) throw [x, y, width, height]
+                rotatedGrid[x].push(tiltedGrid[y][width - x - 1]);
             }
         }
-        grid += '\n';
+        grid = rotatedGrid;
+        
+        let s = '';
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                s += rotatedGrid[x][y]
+            }
+            s += '\n'
+        }
+
+        score = 0;
+        for (let x = 0; x < width; x++) {
+            let colScore = 0
+            for (let y = 0; y < height; y++) {
+                if (grid[x][y] === 1) {
+                    colScore += height - y;
+                }
+            }
+            score += colScore;
+        }
     }
-    // console.log(grid)
+    part2 = score;
+    return [part1, part2];
 }
