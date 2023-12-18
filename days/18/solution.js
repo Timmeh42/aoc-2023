652001cf9b47e5661175a52859b872023e2f6597
timmeh42 <tmeier42+github@gmail.com>
2023-12-18 20:03:08 +0200

    dont need x coord

--- days/18/solution.js
+++ days/18/solution.js
@@ -2,17 +2,14 @@ module.exports = function (input) {
     const instructions = input.trim().split('\n').map((line) => line.split(' '));
 
     // part 1
-    let x = 0;
     let y = 0;
     let fill = 1;
     for (const instruction of instructions) {
         const len = Number(instruction[1])
         if (instruction[0] === 'R') {
-            x += len;
             fill -= len * y;
         }
         if (instruction[0] === 'L') {
-            x -= len;
             fill += len * y;
         }
         if (instruction[0] === 'U') {
@@ -26,18 +23,14 @@ module.exports = function (input) {
     const part1 = fill;
 
     // part 2
-    x = 0;
     y = 0;
-
     fill = 1;
     for (const instruction of instructions) {
         const len = parseInt(instruction[2].slice(2, 7), 16)
         if (instruction[2][7] === '0') {
-            x += len;
             fill -= len * y;
         }
         if (instruction[2][7] === '2') {
-            x -= len;
             fill += len * y;
         }
         if (instruction[2][7] === '3') {
